8ea705f2b2cfe68bed7b81a7fff75bb0cfa6db9a
Jonas Brandel <jonas.brandel@gmail.com>
2019-12-31 12:47:05 +0100

    Comples MessageUrn parsing and reconstruction working

--- src/MassTransit.Tests/MessageUrnSpecs.cs
+++ src/MassTransit.Tests/MessageUrnSpecs.cs
@@ -1,6 +1,7 @@
 namespace MassTransit.Tests
 {
     using System;
+    using System.Diagnostics;
     using NUnit.Framework;
     using TestFramework.Messages;
 
@@ -8,12 +9,66 @@ namespace MassTransit.Tests
     [TestFixture]
     public class MessageUrnSpecs
     {
+        [Test]
+        public void ComplexMessageUrns()
+        {
+            var urn1 = MessageUrn.ForType(typeof(PingMessage));
+            Console.WriteLine($"PingMessage:\r\n\t{urn1.ToString()}\r\n");
+
+            var urn2 = MessageUrn.ForType(typeof(G<X>));
+            Console.WriteLine($"G<X>:\r\n\t{urn2.ToString()}\r\n");
+
+            var urn3 = MessageUrn.ForType(typeof(GX<G<MassTransit.Tests.MessageUrnSpecs.X>>));
+            Console.WriteLine($"GX<G<MassTransit.Tests.MessageUrnSpecs.X>>:\r\n\t{urn3.ToString()}\r\n");
+
+            var urn4 = MessageUrn.ForType(typeof(GM<X,X>));
+            Console.WriteLine($"GM<X,X>:\r\n\t{urn4.ToString()}\r\n");
+
+        }
+
+        //PingMessage:
+        //	urn:message:MassTransit.TestFramework.Messages:PingMessage
+
+        //G<X>:
+        //	urn:message:MassTransit.Tests:G[[MassTransit.Tests:MessageUrnSpecs+X]]
+
+        //GX<G<MassTransit.Tests.MessageUrnSpecs.X>>:
+        //	urn:message:MassTransit.Tests:MessageUrnSpecs+GX[[MassTransit.Tests:G[[MassTransit.Tests:MessageUrnSpecs+X]]]]
+
+        //GM<X, X>:
+        //	urn:message:MassTransit.Tests:GM[[MassTransit.Tests:MessageUrnSpecs+X],[MassTransit.Tests:MessageUrnSpecs+X]]
+
+        [Test]
+        public void GetTypeFromSimpleMessageUrn()
+        {
+            var type1 = MessageUrn.ForMessageUrnString("urn:message:MassTransit.TestFramework.Messages:PingMessage");
+            Assert.AreEqual(typeof(PingMessage), type1);
+
+
+        }
+        [Test]
+        public void GetTypeFromGenericWithNestedTypeMessageUrn()
+        {
+            var type2 = MessageUrn.ForMessageUrnString("urn:message:MassTransit.Tests:G[[MassTransit.Tests:MessageUrnSpecs+X]]");
+            Assert.AreEqual(typeof(G<X>), type2);
+
+        }
+
+        [Test]
+        public void GetTypeFromGenericWithMultipleNestedTypesMessageUrn()
+        {
+            var type2 = MessageUrn.ForMessageUrnString("urn:message:MassTransit.Tests:GM[[MassTransit.Tests:MessageUrnSpecs+X],[MassTransit.Tests:MessageUrnSpecs+X]]");
+            Assert.AreEqual(typeof(GM<X,X>), type2);
+
+        }
 
         [Test]
         public void SimpleMessage()
         {
+
             var urn = MessageUrn.ForType(typeof (PingMessage));
             Assert.AreEqual(urn.AbsolutePath, "message:MassTransit.TestFramework.Messages:PingMessage");
+
         }
 
         [Test]
@@ -38,6 +93,9 @@ namespace MassTransit.Tests
         }
 
         class X{}
+
+        class GX<T> { }
     }
     public class G<T>{}
+    public class GM<T1,T2> { }
 }
--- src/MassTransit/MessageUrn.cs
+++ src/MassTransit/MessageUrn.cs
@@ -2,6 +2,7 @@ namespace MassTransit
 {
     using System;
     using System.Collections.Concurrent;
+    using System.Linq;
     using System.Reflection;
     using System.Runtime.Serialization;
     using System.Text;
@@ -12,7 +13,8 @@ namespace MassTransit
         Uri
     {
         static readonly ConcurrentDictionary<Type, Cached> _cache = new ConcurrentDictionary<Type, Cached>();
-
+        static readonly ConcurrentDictionary<string, Cached> _urnTypeCache = new ConcurrentDictionary<string, Cached>(StringComparer.OrdinalIgnoreCase);
+        static readonly ConcurrentDictionary<string, Type> _messageTypesCache = new ConcurrentDictionary<string, Type>();
         public static MessageUrn ForType<T>() => MessageUrnCache<T>.Urn;
         public static string ForTypeString<T>() => MessageUrnCache<T>.UrnString;
 
@@ -21,14 +23,76 @@ namespace MassTransit
             if (type.ContainsGenericParameters)
                 throw new ArgumentException("A message type may not contain generic parameters", nameof(type));
 
-            return _cache.GetOrAdd(type, _ => (Cached)Activator.CreateInstance(typeof(Cached<>).MakeGenericType(type))).Urn;
+            Cached cached = _cache.GetOrAdd(type, _ => (Cached)Activator.CreateInstance(typeof(Cached<>).MakeGenericType(type)));
+            return _urnTypeCache.GetOrAdd(cached.UrnString, cached).Urn;
         }
 
         public static string ForTypeString(Type type)
         {
-            return _cache.GetOrAdd(type, _ => (Cached)Activator.CreateInstance(typeof(Cached<>).MakeGenericType(type))).UrnString;
+            Cached cached = _cache.GetOrAdd(type, _ => (Cached)Activator.CreateInstance(typeof(Cached<>).MakeGenericType(type)));
+            return _urnTypeCache.GetOrAdd(cached.UrnString, cached).UrnString;
         }
 
+        public static Type ForMessageUrnString(string urnString)
+        {
+            Cached cached = null;
+            if (!_urnTypeCache.TryGetValue(urnString, out cached))
+            {
+                MessageUrn urn = new MessageUrn(urnString);
+                Type type = GetType(urn);
+                cached = _cache.GetOrAdd(type, _ => (Cached)Activator.CreateInstance(typeof(Cached<>).MakeGenericType(type)));
+                _urnTypeCache.TryAdd(cached.UrnString, cached);
+            }
+            return cached.MessageType;
+        }
+        public static Type GetType(MessageUrn urn)
+        {
+            string urnTypeString = urn.GetUrnTypeString();
+            return GetType(urnTypeString);
+        }
+        public static Type GetType(string urnTypeString)
+        {
+            var recipe = Deconstruct(urnTypeString);
+            return BuildCompleteType(recipe);
+        }
+
+        private static Type BuildCompleteType(TypeRecipe recipe)
+        {
+            if (recipe.GenericTypeArguments?.Length > 0)
+            {
+                if (!recipe.Root.IsGenericType)
+                    throw new ArgumentException("Invalid TypeRecipe");
+
+                return recipe.Root.MakeGenericType(recipe.GenericTypeArguments.Select(t => BuildCompleteType(t)).ToArray());
+            }
+            else
+            {
+                return recipe.Root;
+            }
+
+        }
+
+        public static Type GetType(string typeName, bool throwOnError)
+        {
+            if (_messageTypesCache.TryGetValue(typeName, out var foundType))
+                return foundType;
+
+            var type = Type.GetType(typeName);
+            if (type != null) return type;
+            foreach (var a in AppDomain.CurrentDomain.GetAssemblies())
+            {
+                type = a.GetType(typeName, false, true);
+                if (type != null)
+                {
+                    _messageTypesCache.TryAdd(typeName, type);
+                    return type;
+                }
+            }
+
+            if (throwOnError)
+                throw new ArgumentOutOfRangeException($"{typeName} not found in any loaded assembly!");
+            return null;
+        }
 
         static class MessageUrnCache<T>
         {
@@ -47,6 +111,7 @@ namespace MassTransit
         {
             MessageUrn Urn { get; }
             string UrnString { get; }
+            Type MessageType { get; }
         }
 
 
@@ -55,6 +120,7 @@ namespace MassTransit
         {
             public MessageUrn Urn => MessageUrnCache<T>.Urn;
             public string UrnString => MessageUrnCache<T>.UrnString;
+            public Type MessageType => typeof(T);
         }
 
 
@@ -68,36 +134,76 @@ namespace MassTransit
         {
         }
 
+        public string GetUrnTypeString()
+        {
+            if (!string.Equals("urn", Scheme, StringComparison.OrdinalIgnoreCase)
+                || !Segments[0].StartsWith("message:", StringComparison.OrdinalIgnoreCase)
+                || Segments.Length == 0)
+            {
+                throw new ArgumentException("Not a vaild MessageUrn!");
+            }
+
+            return Segments[0].Substring("message:".Length);
+
+        }
         public void Deconstruct(out string name, out string ns, out string assemblyName)
+        {
+            Deconstruct(this.GetUrnTypeString(), out name, out ns, out assemblyName);
+        }
+        public static void Deconstruct(string urnTypeString, out string name, out string ns, out string assemblyName)
         {
             name = null;
             ns = null;
             assemblyName = null;
 
-            if (Segments.Length > 0)
+            string[] names = urnTypeString.Split(':');
+            if (names.Length == 1)
             {
-                string[] names = Segments[0].Split(':');
-                if (string.Compare(names[0], "message", StringComparison.OrdinalIgnoreCase) == 0)
+                name = names[0];
+            }
+            else if (names.Length == 2)
+            {
+                name = names[1];
+                ns = names[0];
+            }
+            else if (names.Length >= 3)
+            {
+                name = names[1];
+                ns = names[0];
+                assemblyName = names[2];
+            }
+
+        }
+        public class TypeRecipe
+        {
+            public Type Root;
+            public TypeRecipe[] GenericTypeArguments;
+        }
+        public static TypeRecipe Deconstruct(string urnTypeString)
+        {
+            //MassTransit.Tests:GM[[MassTransit.Tests:MessageUrnSpecs+X],[MassTransit.Tests:MessageUrnSpecs+X]]
+            int indexOfColon = urnTypeString.IndexOf(':');
+            if (indexOfColon > -1)
+            {
+                string ns = urnTypeString.Substring(0, indexOfColon);
+                string rest = urnTypeString.Substring(indexOfColon + 1);
+                int indexOfGenericsSeparator = rest.IndexOf('[');
+                if(indexOfGenericsSeparator == -1)
                 {
-                    if (names.Length == 2)
-                    {
-                        name = names[1];
-                    }
-                    else if (names.Length == 3)
-                    {
-                        name = names[2];
-                        ns = names[1];
-                    }
-                    else if (names.Length >= 4)
-                    {
-                        name = names[2];
-                        ns = names[1];
-                        assemblyName = names[3];
-                    }
+                    return new TypeRecipe() { Root = GetType($"{ns}.{rest}", true) };
                 }
+                string genericName = rest.Substring(0, indexOfGenericsSeparator);
+                //rest = "G[[MassTransit.Tests:MessageUrnSpecs+X]]"
+                //GM[[MassTransit.Tests:MessageUrnSpecs+X],[MassTransit.Tests:MessageUrnSpecs+X]]
+                string[] nestedTypeStrings = rest.Substring(indexOfGenericsSeparator + 1, rest.Length - (indexOfGenericsSeparator + 2)).Split(',');
+                
+                return new TypeRecipe() { Root = GetType($"{ns}.{genericName}`{nestedTypeStrings.Length}", true), GenericTypeArguments = nestedTypeStrings.Select(s => Deconstruct(s.Substring(1, s.Length-2))).ToArray() };
+            }
+            else
+            {
+                throw new ArgumentException($"Not a valid urnTypeString: {urnTypeString}");
             }
         }
-
         static string GetUrnForType(Type type)
         {
             var sb = new StringBuilder("urn:message:");
